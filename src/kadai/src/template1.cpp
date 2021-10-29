09f47fbfa4d5738ca38558d80af9924b4bf8e9e6
daichiofuchi <dai1011q@gmail.com>
2021-10-30 05:54:54 +0900

    テンプレ1

---
+++ src/kadai/src/template1.cpp
@@ -0,0 +1,23 @@
+#include <ros/ros.h>
+
+#define CTRL_PERIOD 0.02f
+
+inline void インスタンス名(const 受け取りたいrosの型::ConstPtr &msg);//例えばrosのint型は, std_msgs::Int32, となる。
+
+int main( int argc, char **argv ){
+  ros::init( argc, argv, "ノード名" );
+  ros::NodeHandle nh;
+  ros::Publisher インスタンス名 = nh.advertise<送りたいrosの型>("トピック名", 100);
+  ros::Subscriber インスタンス名 = nh.subscribe("トピック名", 100, コールバック関数);
+  ros::Rate loop_rate( 1.0/CTRL_PERIOD );
+  while ( ros::ok()){
+    ros::spinOnce();
+    10行目で宣言したインスタンス名.publish(送りたい変数の名前);
+    loop_rate.sleep();
+  }
+  return 0;
+}
+
+inline void 5行目で宣言したインスタンス名(const 受け取りたいrosの型::ConstPtr &msg){
+    コールバック関数の中身を書く
+}
